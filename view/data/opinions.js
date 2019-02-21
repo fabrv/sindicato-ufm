a994ac017243e84df7e48e7c83a8d41247d479e4
Fabrizzio Rivera <frivera@nevicad.com>
2019-02-21 09:11:41 -0600

    full site

---
+++ view/data/opinions.js
@@ -0,0 +1,9 @@
+opinions = [
+  {
+    "date": "21 de Febrero, 2019",
+    "author": "Fabrizzio Rivera",
+    "headline": "Opinion de prueba",
+    "subhead": "Articulo de prueba para el sindicato UFM",
+    "body": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum ut tristique et egestas. Aliquet enim tortor at auctor urna nunc. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Ipsum nunc aliquet bibendum enim facilisis. Varius morbi enim nunc faucibus a pellentesque. Netus et malesuada fames ac turpis egestas. Lobortis mattis aliquam faucibus purus in massa. In tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Hac habitasse platea dictumst quisque sagittis purus sit. Diam vulputate ut pharetra sit. Vel risus commodo viverra maecenas accumsan lacus vel. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Egestas diam in arcu cursus euismod quis.</p>"
+  }
+]
--- view/main.js
+++ view/main.js
@@ -1,1 +1,60 @@
-console.log(window.location.pathname)
+const path = window.location.pathname.slice(1,window.location.pathname.length)
+let page = 0
+let wrapper = ""
+const maxPage = Math.floor(opinions.length/10)
+
+if(!isNaN(path)){
+  page = parseInt(path)
+}
+if (path === ""){
+  console.log('test')
+  page = 0
+}
+
+if (opinions.length > (page + 1) * 10){
+  document.getElementById('more').style.display = 'inline-block';
+}
+if (page > 0){
+  document.getElementById('less').style.display = 'inline-block';
+}
+
+if (!isNaN(path)){
+  if (path > maxPage){
+    window.location.replace(`/${maxPage}`)
+  }
+
+  for (let i = page * 10; i < (page * 10) + 10; i++){
+    if (i >= opinions.length){
+      i = (page * 10) + 11
+    } else{
+      wrapper += parseArticle(opinions[i].headline, opinions[i].subhead, opinions[i].body, opinions[i].date, opinions[i].author)
+    }
+  } 
+}else {
+  const article = opinions.find(opinion => opinion.headline === decodeURI(path))
+  wrapper += parseArticle(article.headline, article.subhead, article.body, article.date, article.author)
+}
+
+document.getElementById('wrapper').innerHTML = wrapper
+
+function parseArticle(headline, subhead, body, date, author){
+  const article = `
+  <div class="content">
+    <h1><a href="${encodeURI(headline)}">${headline}</a></h1>
+    <p class="info"><b>${author}</b>  -  ${date}</p>
+    <p class="subhead">${subhead}</p>
+    <hr>
+    <div class="body">${body}</div>
+    <hr>
+  </div>
+  `
+  return article
+}
+
+function addPage(){
+  window.location.href = `/${page + 1}`
+}
+
+function lessPage(){
+  window.location.href = `/${page - 1}`
+}
