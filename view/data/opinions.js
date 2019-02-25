35685b5bb8d3eebe9c7a8dbbe6de7255141c85ea
Fabrizzio Rivera <frivera@nevicad.com>
2019-02-25 06:06:58 -0600

    Cambio de titulo

--- view/data/opinions.js
+++ view/data/opinions.js
@@ -2,7 +2,7 @@ opinions = [
   {
     "date": "23 de Febrero, 2019",
     "author": "Fabrizzio Rivera",
-    "headline": "Idiología e hipocresía",
+    "headline": "Hipocresía",
     "subhead": "",
     "body": "<p>“Se sostiene que el creer que a las Universidades compete preocuparse por la solución de los problemas sociales descansa sobre dos <b>ideas equivocadas: a) que la misión de las Universidades es, al menos en parte, política;</b> b) que las Universidades son centros de servicio social.“ <a href='https://www.ufm.edu/images/d/d7/Ideario.pdf'> <i> - Extracto del ideario de la UFM</i> </a> </p><p>Este es una de las ideas fundamentales de la universidad, y aún así se les ha olvidado. Parece que los propósitos se han desvirtuado y la ideología de la libertad se ha convertido en excusas de fascismo y corrupción, que la promoción de la libertad económica se ha convertido en la promoción del desperdicio y despilfarro, y que la <i>verdad y justicia</i> han pasado a segundo plano contra la <i>ideología y política.</i></p><p>Como poder tomar en serio que la universidad no tiene una misión, al menos en parte, política cuando catedráticos y otros miembros importantes de la u rechazan pesquisas a miembros del congreso con la idea de que es «una persecución a <b>la derecha</b>». Como poder tomar en serio que la universidad es contraria a toda orientación dogmática y asegura la enseñanza sin adoctrinación cuando existe el <b>C</b>entro de <b>H</b>Adoctrinación <b>H</b>Avanzado.	</p><p>La universidad ha desvirtuado sus propósitos y para muchos estudiantes la forma de actuar de la u dice más de sus ideales que cualquier clase del CHH.</p>"
   }
