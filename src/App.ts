8a0a87a891151639c6eb44f5c1613620b9048a8d
Fabrizzio Rivera <frivera@nevicad.com>
2019-04-29 19:57:17 -0600

    minor styling changes

--- src/App.ts
+++ src/App.ts
@@ -14,7 +14,7 @@ const pgClient = new Client({
 
 const indexStart = '<!DOCTYPE html><html><head>'
 const indexContent = '<meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-1298512778914438",enable_page_level_ads: true});</script><link rel="stylesheet" type="text/css" media="screen" href="../main.css"><script src="../main.js"></script></head><body><div class="header"><h1>EL SINDICATO</h1><ul class="links"><li><a href="../musica">MÚSICA</a><li><a href="../">OPINIÓN</a></li><li><a href="../nosotros">NOSOTROS</a></li></ul></div><div id="wrapper">'
-const indexEnd = '</div></body></html>'
+const indexEnd = '</div><footer class="header"> <h1>🏛️</h1> <h3>La crítica estudiantil</h3><ul class="links"> <li><a href="../musica">MÚSICA</a><li> <li> <a href="../">OPINIÓN</a> </li> <li> <a href="../nosotros">NOSOTROS</a> </li> </ul> </footer></body></html>'
 
 class App{
   public server: Server
@@ -39,22 +39,10 @@ class App{
 
   mountRoutes(){
     const router: express.Router = express.Router()
-    router.get('json/categories/:category', (req: express.Request, res: express.Response) => {
-      pgClient.query(`SELECT * FROM public."ARTICLE" WHERE category = '${req.params.category}' ORDER BY created DESC`, (error, result) => {
-        if (error) {
-          res.status(500).send(error)
-        } else {
-          let data = []
-          for (let row of result.rows) {
-            data.push(row)
-          }
-          res.status(200).send(data)
-        }
-      })
-    })
 
     router.get('/json/articulo/:article', (req: express.Request, res: express.Response) => {
-      pgClient.query(`SELECT * FROM public."ARTICLE" WHERE "headline" = '${req.params.article}'`, (error, result) => {
+      const article = replaceAll(req.params.article, '_', ' ')
+      pgClient.query(`SELECT * FROM public."ARTICLE" WHERE "headline" = '${article}'`, (error, result) => {
         if (error) {
           res.status(500).send(error)
         } else {
@@ -77,6 +65,20 @@ class App{
       })
     })
     
+    router.get('/json/:category', (req: express.Request, res: express.Response) => {
+      pgClient.query(`SELECT * FROM public."ARTICLE" WHERE category = '${req.params.category}' ORDER BY created DESC`, (error, result) => {
+        if (error) {
+          res.status(500).send(error)
+        } else {
+          let data = []
+          for (let row of result.rows) {
+            data.push(row)
+          }
+          res.status(200).send(data)
+        }
+      })
+    })
+
     router.get('/nosotros', (req: express.Request, res: express.Response) => {
       res.sendFile(path.resolve(__dirname, '../view/nosotros.html'))
     })
@@ -122,13 +124,14 @@ class App{
     router.get('/articulo/:article', (req: express.Request, res: express.Response) => {
       let wrapper: string
       let metaTags: string
+      const pArticle = replaceAll(req.params.article, '_', ' ')
 
-      pgClient.query(`UPDATE public."ARTICLE" SET "views" = "views" + 1 WHERE "headline" = '${req.params.article}'; SELECT * FROM public."ARTICLE" WHERE "headline" = '${req.params.article}';`, (error, result: any) => {
+      pgClient.query(`UPDATE public."ARTICLE" SET "views" = "views" + 1 WHERE "headline" = '${pArticle}'; SELECT * FROM public."ARTICLE" WHERE "headline" = '${pArticle}';`, (error, result: any) => {
         if (error) {
           res.status(500).send(error)
         } else {
           if (result[1].rowCount > 0) {
-            console.log(`Articulo visitado: ${req.params.article}`)
+            console.log(`Articulo visitado: ${pArticle}`)
             
             const article = result[1].rows[0]
             wrapper = parseArticle(article.headline, article.subhead, article.body, article.date, article.author)
@@ -144,7 +147,6 @@ class App{
                       o++
                     }
                     const imgString = article.body.substring(i+1, i+o-1).replace('../', '')
-                    console.log(imgString)
                     metaTags = parseMetaTags(`${article.headline}`, article.subhead, imgString)
                     i = article.body.length
                   }
@@ -215,11 +217,24 @@ class App{
 function parseArticle(headline: string, subhead: string, body: string, date: string, author:string): string{
   const article = `
   <div class="content">
-    <h1><a href="articulo/${encodeURIComponent(headline)}">${headline}</a></h1>
+    <h1><a href="articulo/${encodeURIComponent(replaceAll(headline, ' ', '_'))}">${headline}</a></h1>
     <p class="info"><b>${author}</b>  -  ${date}</p>
     <p class="subhead">${subhead}</p>
     <hr>
     <div class="body">${body}</div>
+    <hr>
+    ¿Te gustó el artículo?
+    <a href="https://www.facebook.com/sharer/sharer.php?u=http://www.sindicato-ufm.com/articulo/${headline}" target="_blank">
+      Comparte en Facebook
+    </a>,
+    <a class="twitter-share-button"
+      href="https://twitter.com/intent/tweet?text=${headline}, http://www.sindicato-ufm.com/articulo/${encodeURIComponent(replaceAll(headline, ' ', '_'))}"
+      data-size="large"
+      target="_blank">
+    Twitter</a>
+    o síguenos en las
+    <a href="https://www.facebook.com/sindicatoUFM" target="_blank">redes</a>
+    <hr>
     <hr>
   </div>
   `
@@ -253,7 +268,7 @@ function parseMetaTags(title: string, description: string, img: string = 'sindic
     <meta property="og:image" content="http://www.sindicato-ufm.com/${img}">
     <meta property="og:type" content="article">
     <meta property="og:locale" content="es_ES">    
-    <meta property="og:url" content="http://www.sindicato-ufm.com/${encodeURIComponent(title)}">
+    <meta property="og:url" content="http://www.sindicato-ufm.com/">
     <meta name="google-site-verification" content="jMeI7ML27XYuoifj0zX0IOkJDRe5qnu0Mv1SI2kUOLI" />
     `
 }
@@ -267,6 +282,10 @@ function spliceSlice(str: string, index: number, count: number, add: any):string
   }
 
   return str.slice(0, index) + (add || "") + str.slice(index + count)
+}
+
+function replaceAll(str: string, find: string, replace: string) {
+  return str.replace(new RegExp(find, 'g'), replace);
 }
 
 //Export app
