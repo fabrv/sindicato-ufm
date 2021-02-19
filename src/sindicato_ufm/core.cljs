8f5144eac89e85855e21901c9f039370e10bb464
fabrv <frivera@nevicad.com>
2021-02-18 22:49:30 -0600

    article template and a little more

--- src/sindicato_ufm/core.cljs
+++ src/sindicato_ufm/core.cljs
@@ -1,16 +1,23 @@
 (ns sindicato-ufm.core
   (:require
    [sindicato-ufm.routes.users :as users]
-   [sindicato-ufm.routes.index :as index]))
+   [sindicato-ufm.routes.index :as index]
+   [clojure.string :as string]))
 
 (def express (js/require "express"))
 (def logger (js/require "morgan"))
+(def exphbs (js/require "express-handlebars"))
 (def app (express))
 
 (defn port []
   (if process.env.PORT process.env.PORT 3000))
 
 (defn static[root] (.static express root))
+
+(defn hbs []
+  (.create exphbs #js{"extname" ".hbs"
+                      "defaultLayout" "layout"
+                      "helpers" #js{"toLink" (fn [context] (string/replace context #" " "-"))}}))
 
 (defn start []
   (->
