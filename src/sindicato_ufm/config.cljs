71daba780da620fb91fd5afe53004df048727485
fabrv <frivera@nevicad.com>
2021-02-22 16:08:55 -0600

    change prod db

--- src/sindicato_ufm/config.cljs
+++ src/sindicato_ufm/config.cljs
@@ -3,7 +3,7 @@
 (def db
   {:connectionString (if (.. js/process -env -DATABASE_URL)
                        (.. js/process -env -DATABASE_URL)
-                       "postgres://yccfbeasuxbqjy:89298dfa903a83f89fb9e4ba35c25584f0363302379a75b7f028711033828ab2@ec2-184-73-153-64.compute-1.amazonaws.com:5432/dfp3idvcven38h")
+                       "postgres://postgres:postgres@postgres:5432/postgres")
    :ssl (if (= (.. js/process -env -DATABASE_URL) "production")
           true
           {:rejectUnauthorized false})})
